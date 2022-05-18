f051de98bfc4e3a38a0eac2ec8910b905c99b9de
Duc Manh <dmanh@kpmg.ca>
2022-05-18 08:30:40 +0000

    Add menu buttons

--- components/Menubar.tsx
+++ components/Menubar.tsx
@@ -8,6 +8,11 @@ export interface MenubarProps {
 const Menubar = ({ title }: MenubarProps) => {
   return (
     <div className={styles.menubar}>
+      <div className={styles.buttons}>
+        <span className={styles.close}></span>
+        <span className={styles.minimize}></span>
+        <span className={styles.fullscreen}></span>
+      </div>
       <h1 className={styles.title}>{title}</h1>
     </div>
   );
