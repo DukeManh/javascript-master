32e7bfd4edec7d21cb2552d744fdeafb7c5f87ee
Duc Manh <dmanh@kpmg.ca>
2022-05-18 04:38:38 +0000

    Log responses history

--- components/Prompt.tsx
+++ components/Prompt.tsx
@@ -1,31 +1,11 @@
 import React, { ChangeEvent, FC, FormEventHandler, KeyboardEventHandler, useState } from 'react';
 import styles from '../styles/Terminal.module.css';
 
-const submitPrompt = async (prompt: string) => {
-  try {
-    const response = await fetch('/api/openai', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        prompt:
-          '\nYou: How do I combine arrays?\nJavaScript chatbot: You can use the concat() method.\n\nYou: How?\n\nJavaScript chatbot:\nJS:var firstArray = [1, 2, 3];\nvar secondArray = firstArray.concat(4);\n\nYou:  How do you make an alert appear after 10 seconds?\n\nJavaScript chatbot:\nJS:var timer = setInterval(function(){\nalert("Hello, world!");\n}, 10);\n\nYou: Find the largest number in an array.\n\nJavaScript chatbot:\n\nvar largestNumber = Math.max(arr[0], arr[1]);\nYou: What is the reduce function used for?\n\nJavaScript chatbot:\n\nThe reduce function is used to reduce an array of data. It takes two arguments: the first is the initial value, and the second is the reducer function.\nYou: Find the median of an array.\n\nJavaScript chatbot:\n\nvar median = arr.reduce(function(a, b) {\nreturn a + b / 2;\n}, 0);\n' +
-          +'You: ' +
-          prompt.trim() +
-          '\n',
-      }),
-    });
-    if (response.ok) {
-      const result = await response.json();
-      console.log(result.choices[0].text);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
+export interface PromptProps {
+  handleSubmit: (prompt: string) => void;
+}
 
-const Prompt: FC = () => {
+const Prompt: FC<PromptProps> = ({ handleSubmit }) => {
   const [prompt, setPrompt] = useState('');
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
@@ -35,19 +15,19 @@ const Prompt: FC = () => {
   const handleKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
     if (e.key === 'Enter' && prompt) {
       e.preventDefault();
-      submitPrompt(prompt);
+      handleSubmit(prompt);
       setPrompt('');
     }
   };
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    submitPrompt(prompt);
+    handleSubmit(prompt);
   };
 
   return (
     <div className={styles.promptWrap}>
-      <form onSubmit={handleSubmit} className={styles.prompt}>
+      <form onSubmit={handleFormSubmit} className={styles.prompt}>
         <label className={styles.label} htmlFor="prompt">
           JS {'>'}
         </label>
--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -1,14 +1,65 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from '../styles/Terminal.module.css';
 
 import Prompt from './Prompt';
 import Window from './Window';
 
+export interface Response {
+  id: string;
+  prompt: string;
+  result: string;
+}
+
+const submitPrompt = async (prompt: string) => {
+  const response = await fetch('/api/openai', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt:
+        '\nYou: How do I combine arrays?\nJavaScript chatbot: You can use the concat() method.\n\nYou: How?\n\nJavaScript chatbot:\nJS:var firstArray = [1, 2, 3];\nvar secondArray = firstArray.concat(4);\n\nYou:  How do you make an alert appear after 10 seconds?\n\nJavaScript chatbot:\nJS:var timer = setInterval(function(){\nalert("Hello, world!");\n}, 10);\n\nYou: Find the largest number in an array.\n\nJavaScript chatbot:\n\nvar largestNumber = Math.max(arr[0], arr[1]);\nYou: What is the reduce function used for?\n\nJavaScript chatbot:\n\nThe reduce function is used to reduce an array of data. It takes two arguments: the first is the initial value, and the second is the reducer function.\nYou: Find the median of an array.\n\nJavaScript chatbot:\n\nvar median = arr.reduce(function(a, b) {\nreturn a + b / 2;\n}, 0);\n' +
+        +'You: ' +
+        prompt.trim() +
+        '\n',
+    }),
+  });
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw new Error('Uncaught Error :(');
+};
+
 const Terminal: FC = () => {
+  const [history, setHistory] = useState<Response[]>([]);
+
+  const handleSubmit = (prompt: string) => {
+    submitPrompt(prompt)
+      .then((result) => {
+        setHistory(
+          history.concat({
+            id: result.id,
+            prompt,
+            result: result.choices[0].text,
+          })
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <Window title="Javascript helper">
       <div className={styles.terminal}>
-        <Prompt />
+        {history.map((response) => (
+          <div key={response.id}>
+            <div>{response.prompt}</div>
+            <div>{response.result}</div>
+          </div>
+        ))}
+        <Prompt handleSubmit={handleSubmit} />
       </div>
     </Window>
   );
