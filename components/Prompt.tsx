f75b1b9550d51cb1efcb8365d880a74f2dcb7f51
Duc Manh <dmanh@kpmg.ca>
2022-05-18 11:28:51 +0000

    Update textarea id to make label happy

--- components/Prompt.tsx
+++ components/Prompt.tsx
@@ -77,6 +77,7 @@ const Prompt = ({
           className={styles.input}
           onChange={handleChange}
           name="prompt"
+          id="prompt"
           value={prompt}
         ></textarea>
         {/* <button type="submit">Submit</button> */}
